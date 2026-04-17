666756081d7f9fa9375b71958c057edb7c925cee
Sandeep-code1234 <12em1a0405@gmail.com>
2026-04-17 23:46:40 +0530

    Adding numbers block

--- Sample1/tempCodeRunnerFile.py
+++ Sample1/tempCodeRunnerFile.py
@@ -4,3 +4,10 @@ print("Hello World!!!")
 
 name = input("Enter your name:")
 print("Hello your name is:",name,"Thanks for your input")
+
+#adding two numbers
+
+n1 = int(input("Entet n1:"))
+n2 = int(input("Enter n2:"))
+sum = n1+n2
+print("Sum of n1 and n2 is :", sum)
